4b35b1bfab26c62ba74a27a7c2faa2633f262afa
EMurrell <82235988+EMurrell@users.noreply.github.com>
2021-08-13 10:49:16 -0400

    Add styles

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+import Layout from "../components/layout";
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About Me">
+      <p>This is another page using the Layout component</p>
+    </Layout>
+  );
+};
+
+export default AboutPage;
