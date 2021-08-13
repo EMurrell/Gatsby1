ac95cdf8def851f49caeffa3a3345d47943bca15
EMurrell <82235988+EMurrell@users.noreply.github.com>
2021-08-13 11:21:06 -0400

    Add image and related plugins

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import * as React from "react";
 import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I made this page using the Layout component</p>
+      <StaticImage alt="My initials" src="../images/em.jpg" />
     </Layout>
   );
 };
